34d41cf61f7c58217dbd3763226264025858936f
Gokulnaath07 <gokuhallows003@gmail.com>
2025-06-13 16:55:24 -0400

    Created Dto for task with image area

---
+++ src/main/java/com/teamseven07/todolist/Model/TaskAndImageDto.java
@@ -0,0 +1,22 @@
+package com.teamseven07.todolist.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskAndImageDto {
+
+    private List<ImageResposeDto> images;
+    private TaskResponseDto tasks;
+
+    private List<ImageRequestDto> imageRequestDto;
+    private TaskRequestDto taskRequestDto;
+}
